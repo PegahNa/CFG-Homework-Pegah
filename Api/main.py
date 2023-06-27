61fd1abbc24eba097319667ac4f723d7992cf3f6
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-27 19:36:43 +0100

    Api exercise setup

--- Api/main.py
+++ Api/main.py
@@ -1,1 +1,19 @@
 from flask import Flask
+
+app = Flask(__name__)
+# -------------------------
+
+
+
+@app.route('/')
+def hello():
+    return {'hello': 'Universe'}
+
+# -------------------------
+# I wonder how this works, maybe I should youtube this later.
+if __name__ == '__main__':
+    # start the app, in debug mode, when the above is true
+    app.run(debug=True)
+    # this code going to run flask
+
+
